fda1a9af17a8d0d39389db4b0146e41b54b00b51
Finn <derfinn@gmail.com>
2014-08-19 13:59:56 +0200

    working merging

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var ldj = require('ldjson-stream')
 var map = require('jsonmap')
 var JSONparse = require('JSONStream').parse
 var toCSV = require('csv-write-stream')
-var join = require('/Users/finn/code/npm/csv-join')
+var join = require('csv-join')
+var gasket = require('gasket')
 
 var baseUrl = 'http://www.berlin.de/badegewaesser/baden-details/index.php/index/'
 
@@ -12,7 +13,7 @@ var measurements =
   request(baseUrl + 'all.json?q=')
     .pipe(JSONparse('index.*'))
     .pipe(ldj.serialize())
-    .pipe(map('{name: this.rss_name, quality: this.wasserqualitaet}'))
+    .pipe(map('{id: this.id, name: this.rss_name, quality: this.wasserqualitaet}'))
     .pipe(toCSV())
 
 var geo = 
@@ -21,28 +22,10 @@ var geo =
     .pipe(ldj.serialize())
     .pipe(map('{coordinates: this.Point.coordinates, name: this.name}'))
     .pipe(toCSV())
-  
-// geo.pipe(process.stdout)
+
 
 join(geo, measurements, {firstColumn: 'name', secondColumn: 'name'})
-  .pipe(toCSV())
+  .pipe(ldj.serialize())
+  .pipe(map('delete this["name-1"]'))
+  .pipe(ldj.serialize())
   .pipe(process.stdout)
-
-  // 
-  // "gasket": {
-  //   "import": [
-  //     "json-merge http://www.berlin.de/badegewaesser/baden-details/index.php/index/all.json?q= --parse=\"index.*\" http://www.berlin.de/badegewaesser/baden-details/index.php/index/all.kml?q= --parse=\"features.*\""
-  //   ],
-  //   "main": [
-  //     "gasket run import",
-  //     "sleep 43200",
-  //     null,
-  //     "gasket run main"
-  //   ],
-  //   "kml": [
-  //     "curl http://www.berlin.de/badegewaesser/baden-details/index.php/index/all.kml?q=",
-  //     "togeojson",
-  //     "jsonfilter ",
-  //     "jsonmap '{geometry: this.geometry.coordinates, rss_name: this.properties.name}'",
-  //     "dat import --json --primary=rss_name"
-  //   ]
