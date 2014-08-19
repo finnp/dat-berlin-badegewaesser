9ed9a24563cfd7efb2a76cbeefb58c345c009a02
Finn <derfinn@gmail.com>
2014-08-19 14:59:42 +0200

    dat hook

---
+++ import.js
@@ -0,0 +1,39 @@
+var request = require('request')
+var xmljson = require('xml-json')
+var ldj = require('ldjson-stream')
+var map = require('through2').obj
+var JSONparse = require('JSONStream').parse
+var toCSV = require('csv-write-stream')
+var join = require('csv-join')
+
+var baseUrl = 'http://www.berlin.de/badegewaesser/baden-details/index.php/index/'
+
+module.exports = function () {
+  var measurements = 
+    request(baseUrl + 'all.json?q=')
+      .pipe(JSONparse('index.*'))
+      .pipe(map(function (obj, enc, next) {
+        next(null, {id: obj.id, name: obj.rss_name, quality: obj.wasserqualitaet})
+      }))
+      .pipe(toCSV())
+
+  var geo = 
+    request(baseUrl + 'all.kml?q=')
+      .pipe(xmljson('Placemark'))
+      .pipe(map(function (obj, enc, next) {
+        next(null, {coordinates: obj.Point.coordinates, name: obj.name})
+      }))
+      .pipe(toCSV())
+
+
+
+  var stream = join(geo, measurements, {firstColumn: 'name', secondColumn: 'name'})
+    .pipe(map(function(obj, enc, next) {
+      delete obj['name-1']
+      next(null, obj)
+    }))
+  
+  return stream
+}
+
+
--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-var request = require('request')
-var xmljson = require('xml-json')
-var ldj = require('ldjson-stream')
-var map = require('jsonmap')
-var JSONparse = require('JSONStream').parse
-var toCSV = require('csv-write-stream')
-var join = require('csv-join')
-var gasket = require('gasket')
+var fetch = require('./import.js')
 
-var baseUrl = 'http://www.berlin.de/badegewaesser/baden-details/index.php/index/'
+module.exports = function (dat, ready) {
+  ready()
+  startFetch()
+  
+  function startFetch() {
+    setTimeout(startFetch, 3000)
+    console.log('Fetching data...')
+    fetch().pipe(dat.createWriteStream({primary: 'id', force: true}))
+  }
 
-var measurements = 
-  request(baseUrl + 'all.json?q=')
-    .pipe(JSONparse('index.*'))
-    .pipe(ldj.serialize())
-    .pipe(map('{id: this.id, name: this.rss_name, quality: this.wasserqualitaet}'))
-    .pipe(toCSV())
-
-var geo = 
-  request(baseUrl + 'all.kml?q=')
-    .pipe(xmljson('Placemark'))
-    .pipe(ldj.serialize())
-    .pipe(map('{coordinates: this.Point.coordinates, name: this.name}'))
-    .pipe(toCSV())
-
-
-join(geo, measurements, {firstColumn: 'name', secondColumn: 'name'})
-  .pipe(ldj.serialize())
-  .pipe(map('delete this["name-1"]'))
-  .pipe(ldj.serialize())
-  .pipe(process.stdout)
+}
